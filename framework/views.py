79a854f8266f3be27e60a2af237d7248a7d151e5
EmanAbuBaker <eman.elsiddig@ms.sd.zain.com>
2021-11-24 08:15:37 +0000

    Create Logging view in Django

--- framework/views.py
+++ framework/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# Logging view in Django :
+# First import the logging library from Python's standard library:
+import os 
+import logging
+# Create a logger for this file or the name of the log level :
+logger = logging.getLogger(__file__)
+# or Get an instance of a logger:
+logger = logging.getLogger(__name__)
